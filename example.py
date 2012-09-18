c811fa7d640fc3c76bae784465c34bda57c973e4
David García Garzón <david.garcia@barcelonamedia.org>
2012-09-18 19:51:06 +0200

    an example of test

---
+++ example.py
@@ -0,0 +1,18 @@
+from b2btest import runBack2BackProgram
+import sys
+
+datapath = "data" # Point it to the directory containing your reference data
+
+runBack2BackProgram(datapath, sys.argv, [
+	('HelloWorld',
+		'echo Hello World > output.txt', [
+			'output.txt',
+		]),
+	('Generate1KHzSine',
+		'sox -n sine.wav synth 1.0 sine  1000.0', [
+			'sine.wav',
+		]),
+
+])
+
+

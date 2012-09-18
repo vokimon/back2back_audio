e10f186cf37aceafb202155a1b316db91bfc2fd5
David García Garzón <david.garcia@barcelonamedia.org>
2012-09-18 15:26:34 +0200

    b2btest installed as package

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+from distutils.core import setup, Extension
+
+setup(
+    name = "b2btest",
+    version = "1.0",
+    description = "Light framework to setup back-to-back test scripts",
+    author = "David Garcia Garzon",
+    author_email = "voki@canvoki.net",
+    url = "https://github.com/vokimon/back2back",
+	packages=['b2btest'],
+    )
+

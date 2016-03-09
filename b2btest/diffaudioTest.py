4a4568a613ff535875adca471b4831ad3a38e88f
David García Garzón <voki@canvoki.net>
2016-03-10 00:24:11 +0100

    relative diffaudio import

--- b2btest/diffaudioTest.py
+++ b2btest/diffaudioTest.py
@@ -19,7 +19,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from diffaudio import *
+from .diffaudio import *
 import unittest
 import numpy as np
 from numpy.testing import assert_equal as np_assert_equal
